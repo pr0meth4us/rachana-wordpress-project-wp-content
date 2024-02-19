d338683233880128d460a2fbfa176dba142f9aa0
pr0meth4us <phearaniron.soeung@gmail.com>
2024-02-19 14:28:22 +0700

    block init

--- plugins/rachana-wordpress-blocks/src/alert/index.js
+++ plugins/rachana-wordpress-blocks/src/alert/index.js
@@ -18,21 +18,16 @@ import './style.scss';
  * Internal dependencies
  */
 import Edit from './edit';
-import save from './save';
+import metadata from './block.json';
 
 /**
  * Every block starts by registering a new block type definition.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-registerBlockType( 'rachana-block/hero', {
+registerBlockType( metadata.name, {
 	/**
 	 * @see ./edit.js
 	 */
 	edit: Edit,
-
-	/**
-	 * @see ./save.js
-	 */
-	save,
 } );
