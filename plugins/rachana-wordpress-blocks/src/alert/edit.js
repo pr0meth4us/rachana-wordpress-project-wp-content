59bda75f01f4212fcedad4907237d3e1691e0038
pr0meth4us <phearaniron.soeung@gmail.com>
2024-02-26 01:20:23 +0700

    alert modulize

---
+++ plugins/rachana-wordpress-blocks/src/alert/edit.js
@@ -0,0 +1,64 @@
+import { PanelBody, PanelRow, SelectControl, TextControl } from '@wordpress/components';
+import { InspectorControls, RichText } from '@wordpress/block-editor';
+
+const edit = (props) => {
+    const { attributes, setAttributes } = props;
+    const { type, message, font } = attributes;
+
+    const onChangeType = (newType) => {
+        setAttributes({ type: newType });
+    };
+
+    const onChangeMessage = (newMessage) => {
+        setAttributes({ message: newMessage });
+    };
+
+    const onChangeFont = (newFont) => {
+        setAttributes({ font: newFont });
+    };
+
+    return (
+        <>
+            <InspectorControls>
+                <PanelBody title="Alert Settings">
+                    <PanelRow>
+                        <SelectControl
+                            label="Type"
+                            value={type}
+                            options={[
+                                { label: 'Primary', value: 'primary' },
+                                { label: 'Secondary', value: 'secondary' },
+                                { label: 'Success', value: 'success' },
+                                { label: 'Danger', value: 'danger' },
+                                { label: 'Warning', value: 'warning' },
+                                { label: 'Info', value: 'info' },
+                                { label: 'Light', value: 'light' },
+                                { label: 'Dark', value: 'dark' },
+                            ]}
+                            onChange={onChangeType}
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <TextControl
+                            label="Font"
+                            value={font}
+                            onChange={onChangeFont}
+                        />
+                    </PanelRow>
+                </PanelBody>
+            </InspectorControls>
+            <div className={`cgds alert alert-${type} fade d-flex align-items-center alert-dismissible-link show`} role="alert" style={{ fontFamily: font }}>
+                <i className={`bi bi-exclamation-circle me-2`}></i>
+                <RichText
+                    tagName="div"
+                    value={message}
+                    onChange={onChangeMessage}
+                    placeholder="Write alert message"
+                />
+                <button type="button" className="btn-close btn-sm" aria-label="Close Alert"></button>
+            </div>
+        </>
+    );
+};
+
+export default edit;
