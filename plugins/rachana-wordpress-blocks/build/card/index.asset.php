44aa541c0e6059cd0c981d668507a3ab29b31939
pr0meth4us <phearaniron.soeung@gmail.com>
2024-02-26 11:03:23 +0700

    card block

--- plugins/rachana-wordpress-blocks/build/card/index.asset.php
+++ plugins/rachana-wordpress-blocks/build/card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'eba8c0b10c17ceb77c86');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'f1a379d1a00e6e7cea26');
