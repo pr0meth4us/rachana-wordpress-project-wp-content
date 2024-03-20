9f1ca85937c97fc2defc4640dd2035415f8e99ca
Virakden <virakden1717@gmail.com>
2024-03-20 16:43:15 +0700

    feat: remove another theme

--- themes/entr/index.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div class="row">
-    <div class="col-md-<?php entr_main_content_width_columns(); ?>">
-        <?php do_action('entr_generate_the_content'); ?>
-    </div>
-    <?php get_sidebar('right'); ?>		
-</div>
-<?php 
-get_footer();
--- themes/entr/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php if (is_active_sidebar('entr-right-sidebar')) { ?>
-    <aside id="sidebar" class="col-md-3">
-        <?php dynamic_sidebar('entr-right-sidebar'); ?>
-    </aside>
-<?php 
-}
