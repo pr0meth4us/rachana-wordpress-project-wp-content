d6ac4ad07f08315bf802a836909ee839b6c590c4
pr0meth4us <phearaniron.soeung@gmail.com>
2024-04-04 19:58:04 +0700

    feat: test

--- update_db.sh
+++ update_db.sh
@@ -4,16 +4,7 @@
 CONTAINER_ID=$(docker-compose ps -q db)
 
 # Export the updated database from the container
-docker exec -i "$CONTAINER_ID" /usr/bin/mysqldump -u root --password=wordpress local > /docker-entrypoint-initdb.d/updated.sql
+#docker exec -i "$CONTAINER_ID" /usr/bin/mysqldump -u root --password=wordpress local > /docker-entrypoint-initdb.d/updated.sql
 
 # Copy the updated SQL file from the container to your local machine
-docker cp "$CONTAINER_ID":/docker-entrypoint-initdb.d/updated.sql ./local.sql
-
-# Commit the updated local.sql to Git
-git add local.sql
-git commit -m "Update database schema and data"
-git push
-
-# Restart the containers
-docker-compose down
-docker-compose up -d
+docker cp "$CONTAINER_ID":/docker-entrypoint-initdb.d/local.sql ./local.sql
