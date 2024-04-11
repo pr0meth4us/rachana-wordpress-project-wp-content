ade814d39c541490340b17dac392ddd8032b0628
pr0meth4us <phearaniron.soeung@gmail.com>
2024-04-11 09:37:12 +0700

    feat: improve interface

--- update_db.sh
+++ update_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Export the updated database from the container add sudo if linux
-sudo docker compose exec db mariadb-dump -u wordpress -pwordpress local > ./local.sql
+docker compose exec db mariadb-dump -u wordpress -pwordpress local > ./local.sql
