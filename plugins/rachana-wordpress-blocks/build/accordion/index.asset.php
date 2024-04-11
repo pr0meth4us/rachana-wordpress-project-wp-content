3a60a5750bb29e36cc0b9eaccee0e193b23c63a6
pr0meth4us <phearaniron.soeung@gmail.com>
2024-04-11 11:33:29 +0700

    feat: improve interface

--- plugins/rachana-wordpress-blocks/build/accordion/index.asset.php
+++ plugins/rachana-wordpress-blocks/build/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'e4847ca219c40ffb74f7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '6e2d04db41277346f3bc');
