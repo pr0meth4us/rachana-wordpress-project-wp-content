2cd36f0d45931a4e687e1d70af360e7acbf8c12e
pr0meth4us <phearaniron.soeung@gmail.com>
2024-04-17 13:41:34 +0700

    feat: improve interface

--- themes/rachana-theme/inc/footer/validations.php
+++ themes/rachana-theme/inc/footer/validations.php
@@ -1,13 +1,13 @@
 <?php
 add_action('customize_save_validation_before', 'validate_customizer_inputs');
 function validate_customizer_inputs($manager) {
-    $email_setting_id = 'email_link'; // Replace with your actual setting ID
+    $email_setting_id = 'email_link';
     add_filter("customize_validate_{$email_setting_id}", 'validate_email_input', 10, 2);
     $url_setting_ids = ['facebook_link', 'linkedIn_link', 'telegram_link', 'tiktok_link', 'youtube_link', 'location_link']; // Replace with your actual setting IDs
     foreach ($url_setting_ids as $setting_id) {
         add_filter("customize_validate_{$setting_id}", 'validate_url_input', 10, 2);
     }
-    $phone_setting_id = 'phone_label'; // Replace with your actual setting ID
+    $phone_setting_id = 'phone_label';
     add_filter("customize_validate_{$phone_setting_id}", 'validate_phone_input', 10, 2);
 }
 
@@ -26,7 +26,6 @@ function validate_url_input($validity, $value) {
 }
 
 function validate_phone_input($validity, $value) {
-    // Adjusted regex to allow for optional spaces and not strictly enforce a specific format, as long as the input does not contain letters
     $phone_regex = "/^(?!.*[a-zA-Z])(\+\d+)?\s*(\(\d+\))?[- \d]*$|^$/";
     if (!empty($value) && !preg_match($phone_regex, $value)) {
         $validity->add('invalid_phone', 'Please enter a valid phone number or leave it blank.');
