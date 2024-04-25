c7e2724d5c861db86205e1fe31069e8a0cc42318
pr0meth4us <phearaniron.soeung@gmail.com>
2024-04-25 13:43:04 +0700

    feat: develop card

--- plugins/rachana-block/src/text-card/save.jsx
+++ plugins/rachana-block/src/text-card/save.jsx
@@ -7,17 +7,21 @@ const save = ({ attributes }) => {
     return (
         <div {...blockProps}>
             {textCard.map((item, index) => (
-                <div className="card" key={index}>
-                    {item.titleTag === 'h1' ? (
-                        <h1 className="card-title" style={{ color: item.titleColor, fontFamily: item.font }}>{item.title}</h1>
-                    ) : (
-                        <h5 className="card-title" style={{ color: item.titleColor, fontFamily: item.font }}>{item.title}</h5>
-                    )}
-                    {item.contentTag === 'strong' ? (
-                        <strong className="card-text" style={{ color: item.contentColor, fontFamily: item.font }}>{item.content}</strong>
-                    ) : (
-                        <a className="card-text" style={{ color: item.contentColor, fontFamily: item.font }}>{item.content}</a>
-                    )}
+                <div className="cgds-example-card-grid">
+                    <div className="cgds card">
+                        <div className="card-body" key={index}>
+                            {item.titleTag === 'h1' ? (
+                                <h1 className="card-title" style={{ color: item.titleColor, fontFamily: item.font }}>{item.title}</h1>
+                            ) : (
+                                <h5 className="card-title" style={{ color: item.titleColor, fontFamily: item.font }}>{item.title}</h5>
+                            )}
+                            {item.contentTag === 'strong' ? (
+                                <strong className="card-text" style={{ color: item.contentColor, fontFamily: item.font }}>{item.content}</strong>
+                            ) : (
+                                <a className="card-text" style={{ color: item.contentColor, fontFamily: item.font }}>{item.content}</a>
+                            )}
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
