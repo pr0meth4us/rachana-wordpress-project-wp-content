46ded89182ccd3dc7f5e4e26f18e75a36d12a457
Virakden <virakden1717@gmail.com>
2024-04-29 16:43:48 +0700

    feat: update link social in footer change to kandal page fb

--- themes/rachana-theme/template-parts/footer/other-links.php
+++ themes/rachana-theme/template-parts/footer/other-links.php
@@ -3,7 +3,7 @@ $links_data = get_theme_mod('links', '');
 $links = json_decode($links_data, true);
 ?>
 <div class="col-lg-3 col-md-6 col-sm-6">
-    <div class="title h3"><?php echo get_theme_mod('links_title', 'បញ្ជាប់ផ្សេងៗ'); ?></div>
+    <div class="title h3"><?php echo get_theme_mod('links_title', 'បញ្ជាប់រហ័ស'); ?></div>
     <ul class="links">
         <?php foreach ($links as $link) : ?>
             <li><a href="<?php echo esc_url($link['url']); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($link['label']); ?></a></li>
