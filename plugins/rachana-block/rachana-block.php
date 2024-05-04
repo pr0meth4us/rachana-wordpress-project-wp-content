1b5ed61c4940855e74be87c9ac5a707f23e38484
pr0meth4us <phearaniron.soeung@gmail.com>
2024-05-04 22:34:10 +0700

    feat: add remove function

--- plugins/rachana-block/rachana-block.php
+++ plugins/rachana-block/rachana-block.php
@@ -27,6 +27,8 @@ function wp_scripts_rachana_wordpress_block_init()
 {
     register_block_type(__DIR__.'/build/card');
     register_block_type(__DIR__.'/build/text-card');
+    register_block_type(__DIR__.'/build/table');
+
 
 }
 add_action('init', 'wp_scripts_rachana_wordpress_block_init');
