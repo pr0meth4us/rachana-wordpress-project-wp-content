803b59c8d305f2243a74f55a6b8ff64f8bda219d
pr0meth4us <phearaniron.soeung@gmail.com>
2024-06-06 09:05:22 +0700

    feat: news blokc

--- plugins/rachana-block/src/news-block/save.js
+++ plugins/rachana-block/src/news-block/save.js
@@ -1,5 +1,8 @@
 import moment from 'moment';
 import { useBlockProps } from "@wordpress/block-editor";
+import ChevronIcon from "../../assets/img/icons/chevron-right-normal.svg";
+import TimeIcon from "../../assets/img/icons/time-icon.svg";
+import PersonIcon from "../../assets/img/icons/person-icon.svg";
 
 const save = ({ attributes }) => {
     const { posts } = attributes;
@@ -22,44 +25,44 @@ const save = ({ attributes }) => {
 
     return (
         <div {...blockProps} id={blockId} className="carousel slide " data-ride="carousel" data-interval="false">
-            <ol className="carousel-indicators">
-                {chunks.map((chunk, index) => (
-                    <li key={index} data-target={`#${blockId}`} data-slide-to={index} className={index === 0 ? 'active' : ''}></li>
-                ))}
-            </ol>
-
             <div className="carousel-inner">
-                    {chunks.map((chunk, chunkIndex) => (
-                        <div key={chunkIndex} className={`carousel-item news-chunk ${chunkIndex === 0 ? 'active' : ''}`}>
-                            {chunk.map((post) => (
-                                <div key={post.key} className="cgds card">
-                                        {post.imageLink && <img className="card-img-top" src={post.imageLink} alt="Post Image"/>}
-                                        <div className="card-body">
-                                            <p className="card-text">
-                                                <small className="text-muted">{getRelativeTime(post.date)}</small>
-                                            </p>
-                                            <a href={post.link} className="card-title link-primary h3"
-                                               dangerouslySetInnerHTML={{__html: post.title.rendered}}></a>
-                                            <p className="card-text">{post.cleanContent}</p>
-                                            <a className="card-link blog-btn" href={post.link}>
-                                                <i className="bi bi-arrow-right-circle-fill"></i>
-                                                ចុចសម្រាប់ទៅទំព័រផ្សេងៗ
-                                            </a>
-                                        </div>
+                {chunks.map((chunk, chunkIndex) => (
+                    <div key={chunkIndex} className={`carousel-item news-chunk ${chunkIndex === 0 ? 'active' : ''}`}>
+                        {chunk.map((post) => (
+                            <div key={post.key} className="cgds card">
+                                {post.imageLink &&
+                                    <img className="card-img-top" src={post.imageLink} alt="Post Image"/>}
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        <img src={TimeIcon} className="slide-show-icon" alt="Time Icon"/>
+                                        <small className="text-muted">{getRelativeTime(post.date)}</small>
+                                    </p>
+                                    <a href={post.link} className="card-title link-primary h3"
+                                       dangerouslySetInnerHTML={{__html: post.title.rendered}}></a>
+                                    <p className="card-text">{post.cleanContent}</p>
+                                    <a className="card-link blog-btn" href={post.link}>
+                                        <i className="bi bi-arrow-right-circle-fill"></i>
+                                        ចុចសម្រាប់ទៅទំព័រផ្សេងៗ
+                                    </a>
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="news-nav d-flex align-items-center justify-content-between">
+                <ol className="carousel-indicators d-flex align-items-center">
+                    {chunks.map((chunk, index) => (
+                        <li key={index} data-target={`#${blockId}`} data-slide-to={index}
+                            className={index === 0 ? 'active' : ''}></li>
                     ))}
+                </ol>
+                <a className="carousel-control-next" href={`#${blockId}`} role="button" data-slide="next">
+                    <img src={ChevronIcon} className="carousel-control-next-icon" alt="Next"
+                         aria-hidden="true"/>
+                </a>
             </div>
 
-            <a className="carousel-control-prev" href={`#${blockId}`} role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href={`#${blockId}`} role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-            </a>
         </div>
     );
 };
