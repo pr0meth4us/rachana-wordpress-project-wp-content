2c9f6beb9c70129e1a3e266d4d32ecd2ee14c3ce
pr0meth4us <phearaneron.soeung@gmail.com>
2024-06-06 03:12:22 +0000

    Feat: Modify api call

--- plugins/rachana-block/src/slide-show/edit.js
+++ plugins/rachana-block/src/slide-show/edit.js
@@ -2,13 +2,11 @@ import { RangeControl } from '@wordpress/components';
 import { useState, useEffect } from '@wordpress/element';
 import { useBlockProps } from '@wordpress/block-editor';
 import apiFetch from '@wordpress/api-fetch';
-import {cleansePostContent} from "../blockHelpers";
+import {cleansePostContent, fetchAuthors, fetchCategories, formatDate} from "../blockHelpers";
+import shortid from "shortid";
 
 const edit = ({ attributes, setAttributes }) => {
     const blockProps = useBlockProps();
-    const [posts, setPosts] = useState([]);
-    const [authors, setAuthors] = useState({});
-    const [categories, setCategories] = useState({});
 
     const fetchPosts = async () => {
         const res = await apiFetch({
@@ -18,47 +16,19 @@ const edit = ({ attributes, setAttributes }) => {
         const categories = await fetchCategories(res);
         const processedPosts = res.map((post) => {
             const { cleanContent, imageLink } = cleansePostContent(post.content.rendered);
-            const formattedDate = new Date(post.date).toLocaleDateString('en-GB', {
-                day: '2-digit',
-                month: 'short',
-                year: 'numeric'
-            }).replace(/ /g, '/');
             return {
                 ...post,
                 author: authors[post.author],
                 categories: post.categories.map((id) => categories[id]),
                 cleanContent,
                 imageLink,
-                formattedDate,
+                date: formatDate(post.date),
                 link: post.link,
+                key: shortid.generate(),
             };
         });
-        setPosts(processedPosts);
-        setAuthors(authors);
-        setCategories(categories);
         setAttributes({ posts: processedPosts });
-    };
-
-    const fetchAuthors = async (posts) => {
-        const authorIds = [...new Set(posts.map((post) => post.author))];
-        const authors = await Promise.all(
-            authorIds.map(async (id) => {
-                const author = await apiFetch({ path: `/wp/v2/users/${id}` });
-                return { [id]: `${author.name} (${author.slug})` };
-            })
-        );
-        return Object.assign({}, ...authors);
-    };
-
-    const fetchCategories = async (posts) => {
-        const categoryIds = [...new Set(posts.flatMap((post) => post.categories))];
-        const categories = await Promise.all(
-            categoryIds.map(async (id) => {
-                const category = await apiFetch({ path: `/wp/v2/categories/${id}` });
-                return { [id]: category };
-            })
-        );
-        return Object.assign({}, ...categories);
+        setAttributes({ id: shortid.generate() });
     };
 
     useEffect(() => {
