e4bddd7a2fce65122ba894396c0ee8c6ec17b040
pr0meth4us <phearaniron.soeung@gmail.com>
2024-06-13 11:26:18 +0700

    feat: modularize code

---
+++ plugins/rachana-block/build/announcement-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '5ae151b2d6ef081844ab');
---
+++ plugins/rachana-block/build/news-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'c1be44d83770bfd80ad9');
